9679adc64835bee9ba3299ed2400bd98fbfe9a0c
Ismaël Héry <ihery@octo.com>
2014-02-21 15:17:50 +0100

    on recupere les Ids des rubriques

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-var tagProposer = require ('./lib/tagsProposer');
+var tagProposer = require('./lib/tagsProposer');
+var async = require('async');
+var lmApi = require('./lib/lmApi');
 
-tagProposer.moreLikeThis('', console.log);
+
+var articlesWithRubs = [];
+
+function getRubriques(article, cb){
+
+   lmApi.getArticle(article.id, function(apiItem){
+
+      var rubriquesIds = apiItem.rubriques.map(function(rub){
+         return rub.id;
+      })
+
+      articlesWithRubs.push({id: article.id, score:article.score, rubIds: rubriquesIds})
+
+      cb(null);
+   });
+};
+
+var articles = tagProposer.moreLikeThis('', function (articles) {
+   console.log(articles);
+
+   async.each(articles,  getRubriques, function(){
+      console.log(articlesWithRubs)
+   });
+
+});
+
