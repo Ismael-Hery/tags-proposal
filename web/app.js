8b14ecea58175b2b811fb4e99f3433ea2c003b56
Ismaël Héry <ihery@octo.com>
2014-02-26 09:15:11 +0100

    ajout d'un seuil de scoring à l'appel

--- web/app.js
+++ web/app.js
@@ -17,7 +17,11 @@ app.post('/rubriques', rubriques);
 
 function rubriques(req, res, next) {
 
-   kNNRubriques.KNearestRubriques(req.body.text, req.body.K,function(rubriques){
+
+   if (req.body.text === undefined || req.body.K === undefined || req.body.threshold === undefined)
+      throw new Error('text and K and threshold must be defined');
+
+   kNNRubriques.KNearestRubriques(req.body.text, req.body.K, req.body.threshold, function (rubriques) {
       console.log('RUBRIQUES', rubriques);
 
       res.send(rubriques);
