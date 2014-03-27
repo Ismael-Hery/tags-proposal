309a83fc84e10deee9363505b4415bdfba7b390e
Ismaël Héry <ihery@octo.com>
2014-03-27 11:47:42 +0100

    suite refacto

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var fs = require('fs');
+var moment = require('moment');
 var mlt = require('./lib/moreLikeThis');
 
 var content = fs.readFileSync('./example.txt').toString();
 
-mlt.moreLikeThis(content, function(result) {
+mlt.moreLikeThis(content, 'depeches', moment().subtract('days', 1), function(result) {
   result.forEach(function(item) {
     console.log('\n', 'score:', item.score, '\n', item.fields);
   });
