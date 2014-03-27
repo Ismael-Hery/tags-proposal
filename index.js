9a3c3b85478475ff1a625b7ed33925638c5306d3
Ismaël Héry <ihery@octo.com>
2014-03-27 11:33:51 +0100

    refacto

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ var mlt = require('./lib/moreLikeThis');
 
 var content = fs.readFileSync('./example.txt').toString();
 
-console.log(content);
+mlt.moreLikeThis(content, function(result) {
+  result.forEach(function(item) {
+    console.log('\n', 'score:', item.score, '\n', item.fields);
+  });
+});
