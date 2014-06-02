4330d8aa0a66aaada1b05584536a5bcc3a1f2a76
Ismaël Héry <ihery@octo.com>
2014-06-02 12:02:57 +0200

    ajout de la reco d'article

---
+++ web/item-document.js
@@ -0,0 +1,12 @@
+{
+  id:12121,
+  date_publication: XXXX,
+  supervised_data: {
+    specified:true,
+    type:'reference'
+  },
+  features:{
+    hasComprendreInTitle:true,
+    atomeCount:5
+  }
+}
